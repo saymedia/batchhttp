503a8282584d204698b72a168e7e2cfda23c170a
Mark Paschal <mark@sixapart.com>
2009-04-20 22:00:47 +0000

    Stub docstrings for batchhttp package Add package metadata

---
+++ __init__.py
@@ -0,0 +1,13 @@
+"""
+
+batchhttp provides parallel fetching of HTTP resources through MIME multipart
+encoding.
+
+"""
+
+__version__ = '1.0'
+__date__ = '20 April 2009'
+__author__ = 'Six Apart, Ltd.'
+__credits__ = """Brad Choate
+Mike Malone
+Mark Paschal"""
