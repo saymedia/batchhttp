bd96f0228ac46103dbca0034b409502d160c8011
Brad Choate <brad@bradchoate.com>
2009-04-22 22:58:07 +0000

    Removed extra ','.

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ made and the batched subresponses are provided to your callbacks.
 
 __version__ = '1.0'
 __date__ = '20 April 2009'
-__author__ = 'Six Apart, Ltd.'
+__author__ = 'Six Apart Ltd.'
 __credits__ = """Brad Choate
 Mike Malone
 Mark Paschal"""
